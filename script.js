02dafa5a2afd729a294e9e18766016d1b105d647
Magnus stampe Nielsen <magnusstampenielsen@Magnuss-MacBook-Pro.local>
2018-10-02 15:22:21 +0200

    earnings

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 //#region DOM content loaded
 "use strict";
 
+let earningsCurrent = 0;
+const earningsGoal = 50000;
+let tapInUse = [false, false, false, false, false, false, false];
+const priceList = {
+  "El Hefe ": 40,
+  "Fairy Tale Ale ": 30,
+  "GitHop ": 45,
+  "Hollaback Lager ": 30,
+  "Hoppily Ever After ": 25,
+  "Mowintime ": 56,
+  "Row 26 ": 45,
+  "Ruined Childhood ": 30,
+  "Sleighride ": 70,
+  "Steampunk ": 60
+};
+
 document.addEventListener("DOMContentLoaded", init);
 //#endregion
 
 //#region init
 function init() {
-  update();
-  //setInterval(update, 2000);
+  setInterval(update, 2000);
 }
 
 //#endregion init
@@ -16,11 +31,43 @@ function init() {
 
 function update() {
   let data = JSON.parse(FooBar.getData());
+  //console.log(data.taps[0].inUse);
+
+  const taps = data.taps;
+
+  taps.forEach(checkTap);
+
   doSomething(data);
 }
 
 function doSomething(dataThing) {
   console.log(dataThing);
 }
+
+function checkTap(tap) {
+  //console.log(tap.id);
+  if (tap.inUse && tap.inUse !== tapInUse[tap.id]) {
+    beerSold(tap.beer);
+    tapInUse[tap.id] = true;
+  } else if (tap.inUse === false) {
+    tapInUse[tap.id] = false;
+  }
+}
+
+//#endregion
+
+//#region goal
+
+//Called when a beer is sold
+function beerSold(beerName) {
+  console.log(beerName);
+  earningsCurrent += priceList[beerName + " "];
+
+  console.log(earningsCurrent);
+
+  updateGoalBar();
+}
+
+function updateGoalBar() {}
 
 //#endregion
