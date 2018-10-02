54ee12d0b3b6641398b95ca19016b6675852e93c
Magnus stampe Nielsen <magnusstampenielsen@Magnuss-MacBook-Pro.local>
2018-10-02 13:45:35 +0200

    Coment regions

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
+//#region DOM content loaded
 "use strict";
 
 document.addEventListener("DOMContentLoaded", init);
+//#endregion
 
+//#region init
 function init() {
-  setInterval(update, 2000);
+  update();
+  //setInterval(update, 2000);
 }
+
+//#endregion init
+
+//#region update cycle
 
 function update() {
   let data = JSON.parse(FooBar.getData());
-  doSomething(data.bartenders);
+  doSomething(data);
 }
 
 function doSomething(dataThing) {
   console.log(dataThing);
 }
+
+//#endregion
